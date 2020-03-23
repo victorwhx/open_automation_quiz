6de56acee442727081a804e51a6329181ea69ff2
WHX <victorwhx@126.com>
2020-03-23 21:52:32 +0800

    Modified quiz1 code with the correct URL.

--- seleniumProject/src/main/java/com/test/script/TestSearch.java
+++ seleniumProject/src/main/java/com/test/script/TestSearch.java
@@ -3,6 +3,7 @@ package com.test.script;
 import com.test.util.BundleUtil;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,6 +18,7 @@ public class TestSearch {
     private WebDriver driver;
     private WebDriverWait wait;
     private BundleUtil bundle;
+    private List<String> itemsList;
 
     @DataProvider(name = "searchItems")
     public static Object[][] words() {
@@ -27,22 +29,16 @@ public class TestSearch {
     }
 
     @Test(dataProvider = "searchItems")
-    public void searchItemsTest(String item) throws InterruptedException {
-        WebElement searchInput = getElement("homePage.searchBox");
+    public void searchItemsTest(String item) {
+        WebElement searchInput = getElements("homePage.searchBox").get(0);
         searchInput.clear();
         searchInput.sendKeys(item);
         searchInput.submit();
 
-        try {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(bundle.getLocator("resultPage.resultItems")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(bundle.getLocator("resultPage.resultItems")));
+        WebElement page2 = getElements("resultPage.page2").get(0);
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-        getElement("resultPage.page2").click();
+        js.executeScript("arguments[0].scrollIntoView();", page2);
+        page2.click();
 
         getResult();
     }
@@ -51,7 +47,7 @@ public class TestSearch {
         List<String> domainList = new ArrayList<>();
         List<WebElement> resultList = getElements("resultPage.resultItems");
 
-        System.out.println("结果列表");
+        itemsList.add("\n结果列表");
         for (WebElement element : resultList) {
             String href = element.getAttribute("href");
             String domainTmp = getDomain(href);
@@ -61,7 +57,7 @@ public class TestSearch {
                 Assert.fail("Failed to get the domain.");
             }
 
-            System.out.println(element.getText() + " ---> " + href);
+            itemsList.add(element.getText() + " ---> " + href);
         }
 
         Map<String, Integer> map = new HashMap<>();
@@ -72,30 +68,17 @@ public class TestSearch {
         Set set = map.entrySet();
         Iterator it1 = map.keySet().iterator();
 
-        System.out.println("");
-        System.out.println("结果统计");
+        itemsList.add("\n结果统计");
         while (it1.hasNext()) {
             Object key = it1.next();
-            System.out.println(key + "：" + map.get(key));
-        }
-    }
-
-    private  WebElement getElement(String key) {
-
-        try {
-            By locator = bundle.getLocator(key);
-            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-            return driver.findElement(locator);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            itemsList.add(key + ": " + map.get(key));
         }
     }
 
     private  List<WebElement> getElements(String key) {
         try {
             By locator = bundle.getLocator(key);
-            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+            wait.until(ExpectedConditions.elementToBeClickable(locator));
             return driver.findElements(locator);
         } catch (Exception e) {
             e.printStackTrace();
@@ -124,7 +107,9 @@ public class TestSearch {
     @BeforeClass
     public void startTest(String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("blink-settings=imagesEnabled=false");
+            driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("ie")) {
             driver = new InternetExplorerDriver();
         } else {
@@ -132,13 +117,26 @@ public class TestSearch {
         }
 
         bundle = new BundleUtil("src/main/resources/application.properties");
+        Point point = new Point(-1000, 0);
+        driver.manage().window().setPosition(point);
         driver.manage().window().maximize();
-        driver.get(bundle.getString("url"));
-        wait = new WebDriverWait(driver, 10);
+        driver.get(bundle.getString("URL"));
+        wait = new WebDriverWait(driver, 20, 200);
+        itemsList = new ArrayList<>();
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        WebElement frame = getElements("homePage.iframe").get(0);
+        wait.until(ExpectedConditions.attributeToBeNotEmpty(frame, "src"));
+        driver.switchTo().frame(frame);
     }
 
     @AfterTest
     public void endTest() {
+        for (String str : itemsList) {
+            System.out.println(str);
+        }
+        itemsList.clear();
         driver.quit();
     }
 }
